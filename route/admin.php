5ef745cc2f086cf2843f1b5985b95df074e47748
leruge <leruge@163.com>
2021-01-18 13:54:23 +0800

    添加路由

---
+++ route/admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @title 后台路由
+ * @author Leruge
+ * @email leruge@163.com
+ * @qq 305530751
+ */
+
+use think\facade\Route;
+
+Route::group('admin', function () {
+    Route::rule('/', 'admin.Login/login'); // 登录页面
+});
