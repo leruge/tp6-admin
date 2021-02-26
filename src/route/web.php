6aa6edc5ada240489a106626dad399295085c488
leruge <leruge@163.com>
2021-02-26 14:06:57 +0800

    修复首页路由

--- src/route/web.php
+++ src/route/web.php
@@ -9,5 +9,5 @@
 use think\facade\Route;
 
 Route::rule('down', 'index.Index/down'); // 下载页面
-Route::rule('down', 'index.Index/index'); // 官网页面
+Route::rule('/', 'index.Index/index'); // 官网页面
 Route::rule('agreement', 'index.Index/agreement'); // 隐私协议
